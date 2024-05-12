7896d4af95c97f2632c48293c1656a3b78085ffd
Channy <beethu@naver.com>
2024-05-12 21:04:17 +0900

    vue_website

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="inner">
+      <h1>
+        <a href="javascript:;">
+          <img :src="logo" alt="Logo" />
+        </a>
+      </h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import logo from "../../assets/logo.svg";
+</script>
